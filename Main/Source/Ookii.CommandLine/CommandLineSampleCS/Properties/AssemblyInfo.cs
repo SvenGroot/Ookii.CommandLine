b6a6fdca54ec1d403b8fec724a494771403fb1a8
Sven Groot <SvenGroot@users.noreply.github.com>
2011-08-01 11:21:19 +0000

    All C# projects use AssemblyInfoCommon.cs

--- Main/Source/Ookii.CommandLine/CommandLineSampleCS/Properties/AssemblyInfo.cs
+++ Main/Source/Ookii.CommandLine/CommandLineSampleCS/Properties/AssemblyInfo.cs
@@ -6,11 +6,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CommandLineSampleCS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Command line parsing sample for Ookii.CommandLine")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ookii.org")]
-[assembly: AssemblyProduct("Ookii.CommandLine")]
-[assembly: AssemblyCopyright("Copyright © Sven Groot (Ookii.org) 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +18,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("45be7e95-80ef-4141-95e5-c46c80e4bef2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
