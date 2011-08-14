a4790037758498ab85b634e9697efd58edc7f5f3
Sven Groot <SvenGroot@users.noreply.github.com>
2011-08-14 05:48:48 +0000

    - Updated license - Added license notices to all files

--- Main/Source/Ookii.CommandLine/Ookii.CommandLine.Tests/CommandLineParserTest.cs
+++ Main/Source/Ookii.CommandLine/Ookii.CommandLine.Tests/CommandLineParserTest.cs
@@ -1,4 +1,14 @@
-﻿using Ookii.CommandLine;
+﻿// Copyright (c) Sven Groot (Ookii.org) 2011
+//
+// This code is published under the Microsoft Public License (Ms-PL).  A copy
+// of the license should be distributed with the code.  It can also be found
+// at http://ookiicommandline.codeplex.com. This notice, the author's name,
+// and all copyright notices must remain intact in all applications,
+// documentation, and source files.
+//
+// I apologize: unit tests for Ookii.CommandLine are not entirely comprehensive.
+// The most important functionality is covered, however. Improved unit tests are on the to do list.
+using Ookii.CommandLine;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Linq;
