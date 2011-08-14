2476487a3bee6bb58830f1330903c2d4f697e7ef
SvenGroot_cp <SND\SvenGroot_cp>
2011-08-14 05:48:48 +0000

    - Updated license - Added license notices to all files

--- Main/Source/Ookii.CommandLine/Ookii.CommandLine.Tests/LineWrappingTextWriterTest.cs
+++ Main/Source/Ookii.CommandLine/Ookii.CommandLine.Tests/LineWrappingTextWriterTest.cs
@@ -1,4 +1,11 @@
-﻿using Ookii.CommandLine;
+﻿// Copyright (c) Sven Groot (Ookii.org) 2011
+//
+// This code is published under the Microsoft Public License (Ms-PL).  A copy
+// of the license should be distributed with the code.  It can also be found
+// at http://ookiicommandline.codeplex.com. This notice, the author's name,
+// and all copyright notices must remain intact in all applications,
+// documentation, and source files.
+using Ookii.CommandLine;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.IO;
--- Main/Source/Ookii.CommandLine/ShellCommandSampleVB/ReadCommand.vb
+++ Main/Source/Ookii.CommandLine/ShellCommandSampleVB/ReadCommand.vb
@@ -1,4 +1,11 @@
-﻿Imports Ookii.CommandLine
+﻿' Copyright (c) Sven Groot (Ookii.org) 2011
+'
+' This code is published under the Microsoft Public License (Ms-PL).  A copy
+' of the license should be distributed with the code.  It can also be found
+' at http://ookiicommandline.codeplex.com. This notice, the author's name,
+' and all copyright notices must remain intact in all applications,
+' documentation, and source files.
+Imports Ookii.CommandLine
 Imports System.ComponentModel
 Imports System.IO
 Imports System.Text
