d4ae0d55cf143e5537c49666a341ee434df3a305
SvenGroot_cp <SND\SvenGroot_cp>
2011-08-31 05:52:09 +0000

    - Multi-value properties can be defined using a read-only property of type that implements ICollection<T> (in addition to the previous array value support) - CommandLineParser constructor throws an exception if a property doesn't have a public set accessor (except for collection types). - Added CommandLineArgument.ElementType property that returns the element type for multi-value properties. - Added CommandLineArgument.IsMultiValue property.

---
+++ Main/Source/Ookii.CommandLine/Ookii.CommandLine/TypeExtensions.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Sven Groot (Ookii.org) 2011
+//
+// This code is published under the Microsoft Public License (Ms-PL).  A copy
+// of the license should be distributed with the code.  It can also be found
+// at http://ookiicommandline.codeplex.com. This notice, the author's name,
+// and all copyright notices must remain intact in all applications,
+// documentation, and source files.
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ookii.CommandLine
+{
+    static class TypeHelper
+    {
+        public static Type FindGenericInterface(Type type, Type interfaceType)
+        {
+            if( type == null )
+                throw new ArgumentNullException("type");
+            if( interfaceType == null )
+                throw new ArgumentNullException("interfaceType");
+            if( !(interfaceType.IsInterface && interfaceType.IsGenericTypeDefinition) )
+                throw new ArgumentException(Properties.Resources.TypeNotGenericInterface, "interfaceType");
+
+            foreach( Type t in type.GetInterfaces() )
+            {
+                if( t.IsGenericType && t.GetGenericTypeDefinition() == interfaceType )
+                    return t;
+            }
+
+            return null;
+        }
+    }
+}
