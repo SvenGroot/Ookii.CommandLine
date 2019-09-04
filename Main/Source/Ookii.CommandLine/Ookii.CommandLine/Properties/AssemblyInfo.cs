105da2d868b28df948f9177fc0ec0ed08f40fcef
Sven Groot <svgroot@corp.microsoft.com>
2019-09-04 16:58:55 -0700

    Add .Net Standard version of the library.

--- Main/Source/Ookii.CommandLine/Ookii.CommandLine/Properties/AssemblyInfo.cs
+++ Main/Source/Ookii.CommandLine/Ookii.CommandLine/Properties/AssemblyInfo.cs
@@ -12,9 +12,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ookii.CommandLine")]
-[assembly: AssemblyDescription("Command line parser for .Net")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
