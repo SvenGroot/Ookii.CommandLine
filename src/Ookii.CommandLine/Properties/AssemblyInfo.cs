6b765777f8fd7591197f1944bda96968e47342bf
Sven Groot <svgroot@microsoft.com>
2022-11-28 15:38:03 -0800

    Refactor common properties into Directory.Build.Props.

--- src/Ookii.CommandLine/Properties/AssemblyInfo.cs
+++ src/Ookii.CommandLine/Properties/AssemblyInfo.cs
@@ -1,19 +1,3 @@
 ﻿using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
-[assembly: System.Resources.NeutralResourcesLanguage("en-US")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8f1476b6-1c84-4ec1-9cda-b80c4cc3fde7")]
