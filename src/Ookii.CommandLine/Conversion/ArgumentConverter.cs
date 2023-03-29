af1f6b0840c8296a82c7743e12f34c266c00ac73
Sven Groot <svgroot@microsoft.com>
2023-03-29 10:07:13 -0700

    Start on new converters.

---
+++ src/Ookii.CommandLine/Conversion/ArgumentConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ookii.CommandLine.Conversion
+{
+    public abstract class ArgumentConverter
+    {
+        public abstract object? Convert(string value, CultureInfo culture);
+
+#if NET6_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        public virtual object? Convert(ReadOnlySpan<char> value, CultureInfo culture)
+        {
+            return Convert(value.ToString(), culture);
+        }
+#endif
+    }
+}
---
+++ src/Ookii.CommandLine/Conversion/ParseConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ookii.CommandLine.Conversion
+{
+    internal class ParseConverter : ArgumentConverter
+    {
+        private readonly MethodInfo _method;
+        private readonly bool _hasCulture;
+
+        public ParseConverter(MethodInfo method, bool hasCulture)
+        {
+            _method = method;
+            _hasCulture = hasCulture;
+        }
+
+        public override object? Convert(string value, CultureInfo culture)
+        {
+            var parameters = _hasCulture
+                ? new object?[] { value, culture }
+                : new object?[] { value };
+
+            try
+            {
+                return _method.Invoke(null, parameters);
+            }
+            catch (CommandLineArgumentException)
+            {
+                throw;
+            }
+            catch (FormatException)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                // Since we don't know what the method will throw, we'll wrap anything in a
+                // FormatException.
+                throw new FormatException(ex.Message, ex);
+            }
+        }
+    }
+}
